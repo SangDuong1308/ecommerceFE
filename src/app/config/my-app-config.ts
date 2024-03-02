b0db6b1f1997a9511106d0f553de1d7684b31aa1
Duong Phuoc Sang <duongphuocsang1308@gmail.com>
2024-03-02 16:48:36 +0700

    Security-LoginLogout

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId : '0oafho2xohqqpoUoR5d7',
+        issuer: 'https://dev-54964998.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email'],
+    }
+
+}
